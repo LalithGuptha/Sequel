9a56fbdce4c675ef0ba5ffbd348add97a0fd6d3b
Rengaraj <renga01042002@gmail.com>
2022-01-07 19:42:15 +0530

    Final Toss

--- Simu/Toss.java
+++ Simu/Toss.java
@@ -1,4 +1,47 @@
 package Simu;
 
+import java.util.Random;
+
 public class Toss {
+    private Team callBy, wonBy, other;
+
+    public Toss(Team callBy, Team wonBy, Team other){
+        setCallBy(callBy);
+        setWonBy(wonBy);
+    }
+    public Team getCallBy() {
+        return callBy;
+    }
+
+    public void setCallBy(Team callBy) {
+        this.callBy = callBy;
+    }
+
+    public Team getWonBy() {
+        return wonBy;
+    }
+
+    public void setWonBy(Team wonBy) {
+        this.wonBy = wonBy;
+    }
+
+    public Team getOther() {
+        return other;
+    }
+
+    public void setOther(Team other) {
+        this.other = other;
+    }
+
+    public Team coinFlip(){
+        Random r = new Random();
+        if(r.nextInt(2)==0){
+            setWonBy(callBy);
+        }
+        else{
+            setWonBy(other);
+        }
+        return getWonBy();
+
+    }
 }
