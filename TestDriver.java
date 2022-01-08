4b2f9944f23f28cdb11964e96bf29f38fae95c7b
Rengaraj <renga01042002@gmail.com>
2022-01-09 02:06:08 +0530

    95% Test Driver done

--- TestDriver.java
+++ TestDriver.java
@@ -2,31 +2,25 @@ import Players.Player;
 import Simu.Team;
 import project.Event;
 import project.Venue;
+
 import java.io.File;
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+
 public class TestDriver {
     public static void main(String[] args) throws IOException {
-        Event e1;
+
         Venue chennai, mumbai,bengaluru, hyderabad, jaipur, kolkata, chandigarh, delhi;
         Team csk, mi, rcb, srh, rr, kkr, kxip, dd;
-        /*String d1, d2, ;
-        LocalDate start, end;
-        try{
-            Scanner s = new Scanner(new File("ExcelFiles//dates.txt"));
-            d1 = s.nextLine();
-            d2 = s.nextLine();
-            start = LocalDate.parse(d1);
-            end = LocalDate.parse(d2);
-            s.close();
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println(MessageFormat.format("Exception arrised {0}", e));
-        }
-*/
 
-        Scanner s1= new Scanner(new File("ExcelFiles//Venue.csv"));
+        Scanner s1= new Scanner(new File("ExcelFiles//event.csv"));
+        Event e1 = new Event(s1.nextLine(), s1.nextLine(), s1.nextLong());
+
+        s1= new Scanner(new File("ExcelFiles//Venue.csv"));
         String[] dummy;
         dummy = (s1.nextLine()).split(",", 0);
         chennai = new Venue(dummy[2], Integer.parseInt(dummy[1]), dummy[0]);
@@ -89,7 +83,19 @@ public class TestDriver {
             }
 
         }
-        System.out.println(players.get(106).getRole());
+        String d1, d2;
+        LocalDate start, end;
+        try{
+            Scanner s = new Scanner(new File("ExcelFiles//dates.txt"));
+            d1 = s.nextLine();
+            d2 = s.nextLine();
+            start = LocalDate.parse(d1);
+            end = LocalDate.parse(d2);
+            s.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(MessageFormat.format("Exception arrised {0}", e));
+        }
     }
 }
 
